d8638925930b9492fc20fe3a9f55116fa01e7445
DJgustavs <gustavszeps2@gmail.com>
2023-11-03 14:40:10 +0200

    2.uzd pabeigts

--- 2uzd.py
+++ 2uzd.py
@@ -2,3 +2,17 @@
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
 """
+
+import feedparser
+
+rss_url="https://news.google.com/home?hl=lv&gl=LV&ceid=LV:lv"
+
+
+kkk=feedparser.parse(rss_url)
+
+for entry in kkk.entries:
+    print("Virsraksts", entry.title)
+    print("saite", entry.link
+    
+    )
+    print("\n")
